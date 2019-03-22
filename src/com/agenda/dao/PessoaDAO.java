50add0152fa49729c3ad745c79b0ef466c7ed612
Enoque Leal <enoque.leal01@etec.sp.gov.br>
2019-03-21 22:17:54 -0300

    Crei a classe DAO

---
+++ src/com/agenda/dao/PessoaDAO.java
@@ -0,0 +1,11 @@
+package com.agenda.dao;
+
+import com.agenda.model.Pessoa;
+
+public class PessoaDAO {
+
+	public void cadastra(Pessoa pessoa) {
+		System.out.println(pessoa.getNome());
+	}
+
+}
--- src/com/agenda/service/CadastraUsuarioService.java
+++ src/com/agenda/service/CadastraUsuarioService.java
@@ -1,11 +1,13 @@
 package com.agenda.service;
 
+import com.agenda.dao.PessoaDAO;
 import com.agenda.model.Pessoa;
 
 public class CadastraUsuarioService {
 
 	public void cadastra(Pessoa pessoa) {
-
+		PessoaDAO dao = new PessoaDAO();
+		dao.cadastra(pessoa);
 	}
 
 }
